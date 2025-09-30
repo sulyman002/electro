b8f6a9fd14efe51ca91508755eee0a4f0de6651e
sulyman002 <oyedelesulaiman@gmail.com>
2025-09-30 18:27:16 +0100

    change the branch to dev

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ElectroLayout from "./ElectroLayout/ElectroLayout";
 import AllCollections from "./Pages/AllCollections";
-import store, { persistor } from "./Redux/store.js";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+
+
+
 
 
 import ProductPage from "./Pages/ProductPage.jsx";
@@ -14,8 +14,8 @@ import Home from "./Pages/Home.jsx";
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
+      
+       
           <Routes>
             <Route path="/" element={<ElectroLayout />}>
               <Route index element={<Home />} />
@@ -24,8 +24,8 @@ function App() {
             </Route>
           </Routes>
           <Toaster position="top-right" />
-        </PersistGate>
-      </Provider>
+        
+      
     </>
   );
 }
